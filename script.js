37e5d848499c5dc85b2aa3ef3767b792c9f4bcc6
ikram <150953076+ikonyx@users.noreply.github.com>
2025-01-31 11:34:21 +0100

    Add a function to populate display

--- script.js
+++ script.js
@@ -29,3 +29,32 @@ function operate(num1, num2, operator) {
         divide(num1, num2);
     }
 }
+
+
+function digitsClick(e) {
+    const display = document.querySelector(".display");
+    const digit = e.target.getAttribute("id");
+    const displayValue = display.textContent;
+
+    if (digit === "." && display.textContent.includes(".")) {
+        return;
+    }
+
+    if (display.textContent.length >= 15) {
+        return;
+    }
+
+    if (display.textContent == "NaN" || display.textContent == "0") {
+        display.textContent = digit;
+    } else {
+        display.textContent += digit;
+    }
+    
+}
+    
+const digits = document.querySelectorAll(".number");
+    digits.forEach(btn => {
+        btn.addEventListener("click", digitsClick);
+    });
+
+    
