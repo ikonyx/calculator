ecf57768927d73d8a55409f7241754ee1fa460ba
ikram <150953076+ikonyx@users.noreply.github.com>
2025-02-01 14:28:21 +0100

    Refactor the code to populate display after clicking on an operator key

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+let num1;
+let num2;
+let ans;
+
+const calculator = {
+    displayValue: "0",
+    firstOperand: null,
+    secondOperand: false,
+    operator: null,
+};
+
+const updateDisplay = () => {
+    const display = document.querySelector(".display");
+    display.textContent = calculator.displayValue;
+}
+
+updateDisplay();
+
+
+
 function add(a, b) {
     return a+b;
 }
@@ -14,10 +34,6 @@ function divide(a,b) {
     return a/b;
 }
 
-let num1;
-let num2;
-let operator;
-
 function operate(num1, num2, operator) {
     if (operator == "+") {
         add(num1, num2);
@@ -30,31 +46,81 @@ function operate(num1, num2, operator) {
     }
 }
 
+const keys = document.querySelector('.container');
+keys.addEventListener('click', (event) => {
+    const { target } = event;
+    if (!target.matches('button')) {
+        return;
+    }
+    
 
-function digitsClick(e) {
-    const display = document.querySelector(".display");
-    const digit = e.target.getAttribute("id");
-    const displayValue = display.textContent;
-
-    if (digit === "." && display.textContent.includes(".")) {
+    if(target.classList.contains('operator')) {
+        handleOperator(target.textContent);
+        updateDisplay();
         return;
     }
 
-    if (display.textContent.length >= 15) {
+    if(target.classList.contains('dot')) {
+        inputDot(target.textContent);
+        updateDisplay();
         return;
     }
 
-    if (display.textContent == "NaN" || display.textContent == "0") {
-        display.textContent = digit;
-    } else {
-        display.textContent += digit;
+    if(target.classList.contains('clear')) {
+        resetDisplay(target.textContent);
+        updateDisplay();
+        return;
     }
-    
-}
-    
-const digits = document.querySelectorAll(".number");
-    digits.forEach(btn => {
-        btn.addEventListener("click", digitsClick);
+
+        inputDigit(target.textContent);
+        updateDisplay();
+
     });
 
-    
+const inputDigit = (digit) => {
+    const { displayValue, secondOperand } = calculator;
+
+    if(secondOperand === true) {
+        calculator.displayValue = digit;
+        calculator.secondOperand = false;   
+    } else {
+        calculator.displayValue = displayValue === "0" ? 
+        digit: displayValue + digit;
+    }
+}
+
+
+const handleOperator = (nextOperator) => {
+    const { firstOperand, displayValue, operator} = calculator;
+    const inputValue = parseFloat(displayValue);
+
+    if (operator && calculator.secondOperand) {
+        calculator.operator = nextOperator;
+        return
+    }
+
+    if (firstOperand == null && inputValue !== NaN ) {
+        calculator.firstOperand = inputValue;
+    } else if (operator) {
+        const ans = operate(firstOperand, inputValue, operator)
+
+        calculator.displayValue = `${parseFloat(ans.toFixed(6))}`;
+        calculator.firstOperand = ans;
+    }
+
+    calculator.secondOperand = true;
+    calculator.operator = nextOperator;
+
+};
+
+const inputDot = (dot) => {
+    if (calculator.secondOperand === true) {
+        calculator.displayValue = "0.";
+        calculator.secondOperand = false;
+        return;
+    }
+
+    if (!calculator.displayValue.includes(dot)) {
+        calculator.displayValue += dot;
+    }
+}
